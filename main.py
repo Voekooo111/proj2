5ddd9c15535d256cd362f41d3625075c75854083
Слатов Илья <slatovil@gmail.com>
2026-03-04 09:17:39 -0500

    Добавлен гороскоп

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import re
+
 def main():
-	date = input("Введите дату (dd-mm-yy): ")
+	date = input("Введите дату (dd.mm.yy): ")
 	try:
-		dd, mm, yy = date.split("-")
+		if re.match(r'\b\d{2}\.\d{2}\.\d{4}\b', date):
+			dd, mm, yy = date.split(".")
+		else:
+			print("Ошибка. Введена неверная дата.")
+		
 	except:
 		print("Ошибка. Введена неверная дата.")
 

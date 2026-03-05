062077823b78b86fbeca16c5c8dd9a5171263893
katapopkova <katapopkova54002@gmail.com>
2026-03-05 10:34:21 -0500

    Добавлен смайл фейс

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from smile import great
 import re
 from goroscope import find as fd
 
 def main():
     date = input("Введите дату (dd.mm.yyyy): ").strip()
-
+    great()
     if not re.fullmatch(r"\d{2}\.\d{2}\.\d{4}", date):
         print("Ошибка. Введена неверная дата.")
         return
